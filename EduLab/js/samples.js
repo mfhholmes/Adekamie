db427e5096dec91abe42091b76ba51b9603973e1
Marcus Holmes <mfhholmes@gmail.com>
2012-10-28 20:25:10 +0800

    samples styling

--- EduLab/js/samples.js
+++ EduLab/js/samples.js
@@ -1,44 +1,57 @@
+samplesModel={
+    "samplesList":[
+        {
+            "name":"Full magazine",
+            "title":"Opens the full Samples magazine",
+            "link":"http://gobrad.wix.com/samplesmag-edition1#!sbowboarding/cvh5",
+            "icon":"data/Samples.png"
+        },
+        {
+            "name":"Snowboarding",
+            "title":"Are you a thrillseeker? Read all about Snowboarding here!",
+            "link":"http://gobrad.wix.com/samplesmag-edition1#!social-media/c1va9",
+            "icon":"data/Samples.png"
+        },
+        {
+            "name":"Social Media",
+            "title":"Are you a Facebook junkie? Essays for and against social media here.",
+            "link":"http://gobrad.wix.com/samplesmag-edition1#!social-media/c1va9",
+            "icon":"data/Samples.png"
+        },
+        {
+            "name":"Year 9 naughtiness",
+            "title":"Are year nines the naughtiest year in your school?",
+            "link":"http://gobrad.wix.com/samplesmag-edition1#!naughty-nines/cvrh",
+            "icon":"data/Samples.png"
+        },
+        {
+            "name":"Body Image",
+            "title":"Have you been teased about the way you look?",
+            "link":"http://gobrad.wix.com/samplesmag-edition1#!teenage-girls/c1ka7",
+            "icon":"data/Samples.png"
+        },
+        {
+            "name":"What teachers think",
+            "title":"Advice on how to improve your essay-writing skills.",
+            "link":"http://gobrad.wix.com/samplesmag-edition1#!teachers-thoughts/c1h9d",
+            "icon":"data/Samples.png"
+        },
+    ]
+};
+
 function addSamples()
 {
+    
 	samples = $("#samplesContainer");
 	$("#samples").width(0).hide().data("status","closed");
-	linktext = 'window.open("http://gobrad.wix.com/samplesmag-edition1#!home/mainPage","Adekamie samples magazine","width=960,height=640,location=no,menubar=no,toolbar=no,status=no")';
-	samples.append("<input type='button' value='Full magazine' onclick='"+linktext+"' title='Opens the full Samples magazine'/><br/>");
-    linktext = 'window.open("http://gobrad.wix.com/samplesmag-edition1#!sbowboarding/cvh5","Adekamie samples magazine","width=960,height=640,location=no,menubar=no,toolbar=no,status=no")';
-    samples.append("<input type='button' value='Snowboarding' onclick='"+linktext+"' title='Are you a thrillseeker? Read all about Snowboarding here!'/><br/>");
-    linktext = 'window.open("http://gobrad.wix.com/samplesmag-edition1#!social-media/c1va9","Adekamie samples magazine","width=960,height=640,location=no,menubar=no,toolbar=no,status=no")';
-    samples.append("<input type='button' value='Social Media' onclick='"+linktext+"' title='Are you a Facebook junkie? Essays for and against social media here.'/><br/>");
-    linktext = 'window.open("http://gobrad.wix.com/samplesmag-edition1#!naughty-nines/cvrh","Adekamie samples magazine","width=960,height=640,location=no,menubar=no,toolbar=no,status=no")';
-    samples.append("<input type='button' value='Year 9 naughtiness' onclick='"+linktext+"' title='Are year nines the naughtiest year in your school?'/><br/>");
-    linktext = 'window.open("http://gobrad.wix.com/samplesmag-edition1#!teenage-girls/c1ka7","Adekamie samples magazine","width=960,height=640,location=no,menubar=no,toolbar=no,status=no")';
-    samples.append("<input type='button' value='Body Image' onclick='"+linktext+"' title='Have you been teased about the way you look?'/><br/>");
-    linktext = 'window.open("http://gobrad.wix.com/samplesmag-edition1#!teachers-thoughts/c1h9d","Adekamie samples magazine","width=960,height=640,location=no,menubar=no,toolbar=no,status=no")';
-    samples.append("<input type='button' value='What teachers think' onclick='"+linktext+"' title='Advice on how to improve your essay-writing skills.'/><img class='sampleSkillIcon' src='./data/skilled_advanced.png'/><br/>");
-}
-function displayComment(that,textToDisplay)
-{
-	//$("#cText").remove();
-	var num = $(".commentText").length;
-	num++;
-	name = "cText" + num;
-	$("#noteZone").append("<div id='"+name+"' class='commentText'>" + textToDisplay + "</div>");
-	$("#"+name).css("top",$(that).position().top);
-}
-function inlineComment(event)
-{
-	target= $(event.target);
-	note = target.attr("note");
-	id=target.attr("ref");
-	if($("#"+id).length > 0)
-		$("#" + id).remove();
-	else
-	{
-		target.append("<div class='note' id="+id+">" + note + "</div>");
-		$("#" + id).css("top",target.position().top + target.height());
+	// add in the open event
+	
+	for(var i = 0; i<samplesModel.samplesList.length;i++){
+	    sample=samplesModel.samplesList[i];
+	    sample.clickButton = function(){
+	        window.open(sample.link,"Adekamie samples magazine","width=960,height=640,location=no,menubar=no,toolbar=no,status=no");
+	    }
 	}
+	ko.applyBindings(samplesModel,samples.get(0));
 }
-function startSamples()
-{
-	// add the click handlers to the annotated spans
-	$(".annotated").on("click",inlineComment);
-}
+
